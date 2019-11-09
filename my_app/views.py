ab9e358acd36e938811c18a3fb18f89617dd4c9d
cayundahenryjr <cayundahenryjr@gmail.com>
2019-11-09 11:45:51 +0800

    Final

--- my_app/views.py
+++ my_app/views.py
@@ -1,6 +1,11 @@
-import request
+import requests
+from requests.compat import quote_plus
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from . import models
+
+BASE_CRAIGLSIST_URL = 'https://manila.craigslist.org/search?query={}'
+
 
 # Create your views here.
 def home(request):
@@ -9,8 +14,33 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
-    print(search)
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGLSIST_URL.format(quote_plus(search))
+    print(final_url)
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+    final_postings = []
+    for post in post_listings:
+
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        final_postings.append((post_title, post_url, post_price))
+        print(post_title)
+        print(post_url)
+        print(post_price)
+
+
     stuff_for_frontend = {
         'search': search,
+        'final_postings': final_postings,
     }
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
